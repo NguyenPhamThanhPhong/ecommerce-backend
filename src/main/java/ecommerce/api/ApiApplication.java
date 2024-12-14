a5fff08e089ba0a865a0140de1fe0428939a29b3
thanhphong17160@gmail.com <thanhphong17160@gmail.com>
2024-12-14 22:01:37 +0700

    pulling to match master & merge

--- src/main/java/ecommerce/api/ApiApplication.java
+++ src/main/java/ecommerce/api/ApiApplication.java
@@ -5,6 +5,9 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+import java.util.Spliterator;
+
 @SpringBootApplication
 @SecurityScheme(
         name = "bearerAuth",
@@ -16,6 +19,9 @@ public class ApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
+        List<Integer> data = List.of(1, 2, 3, 4, 5);
+        data.parallelStream().map(i -> i * 2).forEach(System.out::println);
+        Spliterator<Integer> spliterator = data.spliterator();
     }
 
 }
